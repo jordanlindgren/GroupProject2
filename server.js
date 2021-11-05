776bc6bce2f4cbbdb6bbe83b00695301141c1603
Laura Hofer <ljhofer@gmail.com>
2021-11-05 11:56:02 -0500

    Added a maxAge for the session cookie and deleted extraneous commenting

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    // maxAge: 900
+    maxAge: 900000
   },
   resave: false,
   saveUninitialized: true,
